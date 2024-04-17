5a17f4cdcbf59e847311e6e1301076e54631a9d5
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-17 22:59:11 +0200

    addition of frontend production

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173" || "https://upcyclemyhome.com/";
 
 // Middleware configuration
 module.exports = (app) => {
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
 app.use(cors());
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:5173');
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:5173', "https://upcyclemyhome.com/");
     if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
         return res.status(200).json({});
