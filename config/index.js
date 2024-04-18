6d23ab5125c3fb4fd014e7b85032e5f10d19fa02
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-18 11:44:54 +0200

    changes in cors to accept data from production

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ const cookieParser = require("cookie-parser");
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173" || "https://upcyclemyhome.com";
+const FRONTEND_URL =
+  process.env.ORIGIN || "http://localhost:5173" || "https://upcyclemyhome.com";
 
 // Middleware configuration
 module.exports = (app) => {
@@ -22,16 +23,26 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-app.use(cors());
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:5173', "https://upcyclemyhome.com/");
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
+  app.use(
+    cors({
+      origin: ["https://upcyclemyhome.com", "http://localhost:5173"], // adjust as necessary
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      allowedHeaders: "Content-Type, Authorization",
+    })
+  );
+  app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
+      "Origin, X-Requested-with, Content_Type,Accept,Authorization",
+      "http://localhost:5173",
+      "https://upcyclemyhome.com/"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+      return res.status(200).json({});
     }
     next();
-});
-
+  });
 
   // In development environment the app logs
   app.use(logger("dev"));
