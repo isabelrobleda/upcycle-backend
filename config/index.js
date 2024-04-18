37f4ef1f140cf5b7e2b8d9d1625397dbfcd9fc35
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-18 12:02:02 +0200

    changes in cors to try to fix bug in production

--- config/index.js
+++ config/index.js
@@ -30,19 +30,19 @@ module.exports = (app) => {
       allowedHeaders: "Content-Type, Authorization",
     })
   );
-  app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
-      "Origin, X-Requested-with, Content_Type,Accept,Authorization",
-      "http://localhost:5173",
-      "https://upcyclemyhome.com/"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header(
+  //     "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
+  //     "Origin, X-Requested-with, Content_Type,Accept,Authorization",
+  //     "http://localhost:5173",
+  //     "https://upcyclemyhome.com/"
+  //   );
+  //   if (req.method === "OPTIONS") {
+  //     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+  //     return res.status(200).json({});
+  //   }
+  //   next();
+  // });
 
   // In development environment the app logs
   app.use(logger("dev"));
