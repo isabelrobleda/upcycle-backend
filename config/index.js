6de2af5ccf2df642229c96655d9581e2b384f3a9
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-18 17:15:58 +0200

    changes in middleware

--- config/index.js
+++ config/index.js
@@ -1,54 +1,30 @@
-// We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
-
-// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
 const logger = require("morgan");
-
-// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
-// https://www.npmjs.com/package/cookie-parser
 const cookieParser = require("cookie-parser");
-
-// ℹ️ Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
-// unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL =
-  process.env.ORIGIN || "http://localhost:5173" || "https://upcyclemyhome.com";
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173" || "https://upcyclemyhome.com";
 
 // Middleware configuration
 module.exports = (app) => {
-  // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
 
-  // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      // origin: ["https://upcyclemyhome.com", "http://localhost:5173"], // adjust as necessary
-      // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      // allowedHeaders: "Content-Type, Authorization",
-    })
-  );
-  app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
-      "Origin, X-Requested-with, Content_Type,Accept,Authorization",
-      "http://localhost:5173",
-      "https://upcyclemyhome.com"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
+  // Enabling CORS properly
+  app.use(cors({
+    origin: [FRONTEND_URL], // This assumes FRONTEND_URL is an array which it currently is not; adjust accordingly.
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  }));
 
-  // In development environment the app logs
   app.use(logger("dev"));
-
-  // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+
+  // Error handling middleware should be last, after all other middleware and routes
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 };
