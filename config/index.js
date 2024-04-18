87673c01b0dc2b21517fcccab75a8de06c391d8c
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-18 23:19:11 +0200

    changes in index.js

--- config/index.js
+++ config/index.js
@@ -1,34 +1,43 @@
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
-const app = express();
 
-module.exports = function(app) {
+// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
+// https://www.npmjs.com/package/morgan
+const logger = require("morgan");
 
-  // Middleware Configuration
-  app.use(cors({
-    origin: ['https://upcyclemyhome.com', 'https://localhost:5005'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
+// https://www.npmjs.com/package/cookie-parser
+const cookieParser = require("cookie-parser");
 
+// ℹ️ Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
+// unless the request is made from the same domain, by default express wont accept POST requests
+const cors = require("cors");
+
+const FRONTEND_URL = process.env.ORIGIN || "https://localhost:5173";
+
+// Middleware configuration
+module.exports = (app) => {
+  // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
+  // Services like Fly use something called a proxy and you need to add this to your server
+  app.set("trust proxy", 1);
+
+  // controls a very specific header to pass headers from the frontend
+app.use(cors());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:5173');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+  // In development environment the app logs
   app.use(logger("dev"));
+
+  // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  
-  // Error Handling Middleware
-  // Correctly implemented as it now correctly uses four parameters
-  app.use((err, req, res, next) => {
-    // Check if the headers have already been sent to the client
-    if (res.headersSent) {
-      // If headers are sent, delegate to the default Express error handler
-      return next(err);
-    }
-    // Otherwise, log the error and send an appropriate server error response
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-}
+};
