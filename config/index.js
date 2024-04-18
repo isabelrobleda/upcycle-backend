cf124ff52c7338524a4c5d3260a85d2657bbf86b
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-18 20:36:29 +0200

    fixing on major bug n https and headers

--- config/index.js
+++ config/index.js
@@ -1,30 +1,45 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
-const FRONTEND_URL = process.env.ORIGIN  || "https://upcyclemyhome.com";
+// App Initialization
+const app = express();
+app.set("trust proxy", 1);
 
-// Middleware configuration
-module.exports = (app) => {
-  app.set("trust proxy", 1);
+// Load SSL/TLS Certificates
+const key = fs.readFileSync(path.join(__dirname, 'localhost-key.pem'), 'utf8');
+const cert = fs.readFileSync(path.join(__dirname, 'localhost.pem'), 'utf8');
 
-
+// CORS Configuration
 app.use(cors({
-  origin: 'https://upcyclemyhome.com' || "http://localhost:5173", // Point to the frontend URL
-  credentials: true, // If you're dealing with cookies
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed request methods
-  allowedHeaders: ['Content-Type', 'Authorization'] // Allowed request headers
+  origin: ['https://upcyclemyhome.com', 'http://localhost:5173'], // Corrected to include both origins
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-  app.use(logger("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+// Other Middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-  // Error handling middleware should be last, after all other middleware and routes
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-};
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// HTTPS Server Setup
+const httpsOptions = { key, cert };
+const server = https.createServer(httpsOptions, app);
+
+// Start Server
+const PORT = process.env.PORT || 5005;
+server.listen(PORT, () => {
+  console.log(`Server listening on https://localhost:${PORT}`);
+});
