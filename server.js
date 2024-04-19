e807251ae1f258b0ece99f2490277fefd013db48
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-19 15:56:51 +0200

    Simplify server setup for Heroku

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 const app = require('./app');
-const https = require('https');
-const fs = require('fs');
 const PORT = process.env.PORT || 5005; // Use the PORT environment variable or default to 5005
 
-// Check if running in a secure environment and certificates are available
-if (process.env.NODE_ENV === 'production' && fs.existsSync('server.key') && fs.existsSync('server.cert')) {
-  const options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  };
-  https.createServer(options, app).listen(PORT, () => {
-    console.log(`HTTPS Server running on port ${PORT}`);
-  });
-} else {
-  app.listen(PORT, () => {
-    console.log(`HTTP Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
