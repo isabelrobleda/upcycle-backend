613873b0fedfce99e85505bfc98410b6a13053b9
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-21 18:09:16 +0200

    add procfile

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5005; // Use the PORT environment variable or default to 5005
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html')),
+  function (err){
+    if(err){
+      res.status(500).send(err)
+    }}
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
