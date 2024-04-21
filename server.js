50f2fabc9262cd4eba5a3b76492cfc6c7d99ac0f
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-21 18:45:24 +0200

    addition of get to test

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5005; // Use the PORT environment variable or default to 5005
 
-
+  // Inside your main server file, after setting up middleware but before starting the server
+  app.get('/', (req, res) => {
+    res.status(200).send('Upcycle Backend is running!');
+  });
+  
+    
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
